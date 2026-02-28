f1435c1b43c91c410073a980d77a734e1fe768f2
Rameez329732 <156177291+Rameez329732@users.noreply.github.com>
2026-02-28 18:14:51 +0500

    hero section complete

--- Javascripts/main.js
+++ Javascripts/main.js
@@ -1,8 +1,13 @@
+let menu = document.querySelector("#menu");
+let screen = document.querySelector("#full-navbar");
+
 let navbar = document.querySelector("nav");
-let input = document.querySelector("#input");
 let icon = document.querySelectorAll("nav i");
 let btn = document.querySelector("#btn");
 let btnlink = document.querySelector("#btn a");
+
+let mediaQuery = window.matchMedia("(min-width: 768px)");
+
 
 
 
@@ -10,20 +15,51 @@ window.addEventListener("scroll", function () {
 
     if (window.scrollY > 70) {
         navbar.style.backgroundColor = "lightgray";
-        icon.forEach( function(i){
+        icon.forEach(function (i) {
             i.style.color = "#000";
         })
-        input.style.backgroundColor = "#666666";
         btn.style.backgroundColor = "#666666";
     }
     if (window.scrollY <= 70) {
         navbar.style.backgroundColor = "transparent";
-        icon.forEach( function(i){
+        icon.forEach(function (i) {
             i.style.color = "#dadada";
         })
-        // btnlink.style.color = "#000";
-        input.style.backgroundColor = "transparent";
-        // btn.style.backgroundColor = "transparent";
+
+    }
+});
+
+
+let flag = true
+menu.addEventListener('click', function () {
+
+
+
+
+
+    if (flag) {
+        menu.style.color = "#000000";
+        icon.forEach(function (i) {
+            i.style.color = "#000";
+        })
+        screen.style
+        menu.style.transition = "0.4s ease";
+        menu.classList.remove("ri-menu-line");
+        menu.classList.add("ri-close-large-fill");
+        screen.style.top = "0";
+        screen.style.backgroundColor = "#dadada";
+        flag = false;
+    } else {
+        menu.style.color = "#dadada";
+        icon.forEach(function (i) {
+            i.style.color = "#dadada";
+        })
+        screen.style.backgroundColor = "#dadada";
+        menu.classList.remove("ri-close-large-fill");
+        menu.classList.add("ri-menu-line");
+        screen.style.top = "-100%";
+        flag = true;
     }
 
+    
 });
